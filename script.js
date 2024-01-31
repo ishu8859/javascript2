5061d8f3df34965ae033ba3e09f4d27d85aae2e0
Ishu <kashyapishwar52@gmail.com>
2024-01-31 13:31:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
 let display = document.getElementById('display');
 let expression = '';
 
-function appendToDisplay(value) {
-    expression += value;
-    display.value = expression;
-}
-
-function clearDisplay() {
-    expression = '';
-    display.value = '';
-}
-
-function calculate() {
-    try {
-        expression = eval(expression);
-        display.value = expression;
-    } catch (error) {
-        display.value = 'Error';
-    }
-}
-
 // Add this code to your existing JavaScript file
 function calculateFactorial() {
     let inputNumber = document.getElementById('number').value;
